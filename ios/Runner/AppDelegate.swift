f99bf8d8de234143f8378ec68aad3a73d357d596
mahmoudeng20-ops <mahmoud.eng20@gmail.com>
2026-05-25 11:46:52 +0300

    Regenerate iOS folder and restore Firebase config

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import Flutter
 import UIKit
-import FirebaseCore // 1. أضفنا سطر الاستيراد من الفايربيز هنا
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -8,9 +7,6 @@ import FirebaseCore // 1. أضفنا سطر الاستيراد من الفاير
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    
-    FirebaseApp.configure() // 2. أضفنا سطر التشغيل هنا قبل الـ return ليعمل الفايربيز فور إقلاع التطبيق
-    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
